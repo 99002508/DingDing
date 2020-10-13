98c14e436edab3011323f3e03178addaf0b1e3f1
99002543 <71690428+99002543@users.noreply.github.com>
2020-10-13 12:53:23 +0530

    chat window features added

--- client.py
+++ client.py
@@ -1,22 +1,51 @@
 import socket
-
+import os
+import time
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
-    port = 5003  # socket server port number
+    port = 5013  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
-    print("Are you 18+ ???");
+    print("Are you 18 year above");
+   
+
     message = input(" -> ")  # take input
 
     while message.lower().strip() != 'no':
         client_socket.send(message.encode())  # send message
+        print("--------we are with 3 packages--------");
+        print("MIN----MAX----PRO");
+        print("select package");
+        message = input(" -> ")  # again take input #sending package from client to server
+        client_socket.send(message.encode())
         data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
+        print('Received from server: ' + data)  # show in terminal        
+        #data = client_socket.recv(1024).decode()
+        #print(' ' + data)  #recieving amount of particular package
+        print("press 'ok' to buy the package");
+        response=input(" -> ")
+        client_socket.send(response.encode()) #sending ok from client to server 
+        bill= client_socket.recv(1024).decode()
+        print(bill); #recieving bill
+        
+        '''if(inv=client_socket.recv(1024).decode()):
+        	print("Choose correct package
+        print(inv);'''
+        print("opening chat window for your fav");
+        time.sleep(1);
+        os.system('clear')
+        print("-----connected-----Start DingDing-------");
+       #if (response==min)
+        for i in range(10):
+        	chat=input(" -> ")
+        	client_socket.send(chat.encode())
+        	chat_rec=client_socket.recv(1024).decode()
+        	print(chat_rec) 
+        time.sleep(5);
+        
+        #message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5003 # initiate port no above 1024
+    port = 5013 # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -17,12 +17,56 @@ def server_program():
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
+        
         if not data:
             # if data is not received break
             break
         print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        data = conn.recv(1024).decode() # Recieveing min/max/pro
+        print(" " + str(data))
+        package=str(data);
+        if(package.lower().strip()=='min'):
+                rate_min="you have to pay 100 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package")
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 100 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        if(package.lower().strip()=='max'):
+                rate_min="you have to pay 200 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 200 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        if(package.lower().strip()=='pro'):
+                rate_min="you have to pay 300 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 300 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        '''else:
+        	inv="invalid";
+        	conn.send(inv.encode());'''
+        	
+                
+        for i in range(10):
+          	chat_rec=conn.recv(1024).decode()
+          	print(chat_rec)
+          	chat=input(" -> ")
+          	conn.send(chat.encode());
+          	
+        	
+        	 
+        #data = input(' -> ')
+       # conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
