827bc01ced2568673aabf851531aed196f89cebb
99002561 <71540891+99002561@users.noreply.github.com>
2020-10-13 18:12:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,10 @@ from server import *
 from client import *
 
 class ServerTest(unittest.TestCase):
-	def my_test(self):
-		self.assertEqual(server_program(),"done")
-		
+    def my_test(self):
+        self.assertEqual(server_program(),"done")		
 class ClientTest(unittest.TestCase):
-	def my_test1(self):
-		self.assertEqual(client_program(),"done")
+    def my_test1(self):
+        self.assertEqual(client_program(),"done")
 if __name__ == '__main__':
     unittest.main()
