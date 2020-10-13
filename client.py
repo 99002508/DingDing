e09d80c681e01ab132cd55cd134c4c5b687aa690
99002508 <ashutosh.mishra@ltts.com>
2020-10-13 11:43:56 +0530

    18+ functionality rebuild with minor message

--- client.py
+++ client.py
@@ -3,19 +3,20 @@ import socket
 
 def client_program():
     host = socket.gethostname()  # as both code is running on same pc
-    port = 5003  # socket server port number
+    port = 5012  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
-    print("Are you 18+ ???");
-    message = input(" -> ")  # take input
-
+    print("Are you 18+ ???")
+    message = input(" -> ") 
+    client_socket.send(message.encode())  # send message
+    data = client_socket.recv(1024).decode()  # receive response
+    print(data)
+    
     while message.lower().strip() != 'no':
         client_socket.send(message.encode())  # send message
         data = client_socket.recv(1024).decode()  # receive response
-
         print('Received from server: ' + data)  # show in terminal
-
         message = input(" -> ")  # again take input
 
     client_socket.close()  # close the connection
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import socket
+import sys
 
 
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5003 # initiate port no above 1024
+    port = 5012 # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -20,9 +21,13 @@ def server_program():
         if not data:
             # if data is not received break
             break
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        conn.send(data.encode())  # send data to the client
+        #print("from connected user: " + str(data))
+        elif (data.lower().strip() == 'no'):
+            data="You are Minor."
+            conn.send(data.encode())
+        else:
+            data = input(' -> ')
+            conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
@@ -50,25 +55,16 @@ the program"""
 def remove(connection): 
     if connection in list_of_clients: 
         list_of_clients.remove(connection) 
-  
-while True: 
-  
-    """Accepts a connection request and stores two parameters,  
-    conn which is a socket object for that user, and addr  
-    which contains the IP address of the client that just  
-    connected"""
-    conn, addr = server.accept() 
-  
-    """Maintains a list of clients for ease of broadcasting 
-    a message to all available people in the chatroom"""
-    list_of_clients.append(conn) 
-  
-    # prints the address of the user that just connected 
-    print(addr[0] + " connected")
-  
-    # creates and individual thread for every user  
-    # that connects 
-    start_new_thread(clientthread,(conn,addr))     
-  
-conn.close() 
-server.close() 
+try:  
+    while True:
+       conn, addr = server.accept()
+       list_of_clients.append(conn)
+       print(addr[0] + " connected")
+       start_new_thread(clientthread,(conn,addr))
+       conn.close()
+       server.close()   
+
+except NameError as e:
+    print("Client is a minor") 
+
+
