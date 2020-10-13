f95a28c5b657ba1c35f840b1126b406ae7f434a4
99002561 <71540891+99002561@users.noreply.github.com>
2020-10-13 14:46:10 +0530

    File handling feature added
    For item retrival

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import socket
-import sys
 
 
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5012 # initiate port no above 1024
+    port = 5013 # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
@@ -18,16 +17,69 @@ def server_program():
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
         data = conn.recv(1024).decode()
+        
         if not data:
             # if data is not received break
             break
-        #print("from connected user: " + str(data))
-        elif (data.lower().strip() == 'no'):
-            data="You are Minor."
-            conn.send(data.encode())
-        else:
-            data = input(' -> ')
-            conn.send(data.encode())  # send data to the client
+        print("from connected user: " + str(data))
+        data = conn.recv(1024).decode() # Recieveing min/max/pro
+        print(" " + str(data))
+        package=str(data);
+        if(package.lower().strip()=='min'):
+                rate_min="you have to pay 100 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package")
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 100 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        if(package.lower().strip()=='max'):
+                rate_min="you have to pay 200 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 200 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        if(package.lower().strip()=='pro'):
+                rate_min="you have to pay 300 rs per month"
+                conn.send(rate_min.encode());
+                ok_rec=conn.recv(1024).decode() #recieving 'ok' from client for min package
+                print(str(ok_rec))
+                bill="---------BILL-------->"\
+                "received 300 rs "\
+                "happy DingDing."
+                conn.send(bill.encode());
+        '''else:
+        	inv="invalid";
+        	conn.send(inv.encode());'''
+        	
+        if(package=="min"):     
+        	for i in range(3):
+          		chat_rec=conn.recv(1024).decode()
+          		print(chat_rec)
+          		chat=input(" -> ")
+          		conn.send(chat.encode());
+        if(package=="max"):     
+        	for i in range(5):
+          		chat_rec=conn.recv(1024).decode()
+          		print(chat_rec)
+          		chat=input(" -> ")
+          		conn.send(chat.encode());
+        if(package=="pro"):     
+        	for i in range(10):
+          		chat_rec=conn.recv(1024).decode()
+          		print(chat_rec)
+          		chat=input(" -> ")
+          		conn.send(chat.encode());
+         
+          	
+        	
+        	 
+        #data = input(' -> ')
+       # conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
@@ -35,9 +87,6 @@ def server_program():
 if __name__ == '__main__':
     server_program()
   
-"""Using the below function, we broadcast the message to all 
-clients who's object is not the same as the one sending 
-the message """
 def broadcast(message, connection): 
     for clients in list_of_clients: 
         if clients!=connection: 
@@ -49,22 +98,18 @@ def broadcast(message, connection):
                 # if the link is broken, we remove the client 
                 remove(clients) 
   
-"""The following function simply removes the object 
-from the list that was created at the beginning of  
-the program"""
 def remove(connection): 
     if connection in list_of_clients: 
-        list_of_clients.remove(connection) 
-try:  
+        list_of_clients.remove(connection)
+try:
     while True:
-       conn, addr = server.accept()
-       list_of_clients.append(conn)
-       print(addr[0] + " connected")
-       start_new_thread(clientthread,(conn,addr))
-       conn.close()
-       server.close()   
-
+        conn, addr = server.accept()
+        list_of_clients.append(conn)
+        print(addr[0] + " connected")
+        start_new_thread(clientthread,(conn,addr))
+        conn.close()
+        server.close()      
 except NameError as e:
-    print("Client is a minor") 
+	print("Client is minor")     
 
 
